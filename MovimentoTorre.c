da3d41deb7ade93668bc2256a16c105ec7457595
Lucas-creat <lucas_b7@hotmail.com>
2025-06-04 01:21:04 +0000

    Tema3nivelnovato

---
+++ MovimentoTorre.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+    int i = 0;
+
+    while (i <= 5)
+    {
+        if (i != 5)
+        {
+            printf("Torre direita\n");
+        }       
+        i++;
+    }
+
+return 0;
+
+}
