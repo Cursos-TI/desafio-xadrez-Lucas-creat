659d0eede1ad89a1975d728bc11661e414a97fe2
Lucas-creat <lucas_b7@hotmail.com>
2025-06-05 00:00:28 +0000

    inclusao movimento cavalo

---
+++ tema3nivelAventureiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+    int cavalo = 1;
+
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("cima \n");
+        }
+        printf("esquerda");
+    }
+
+return 0;
+
+}
